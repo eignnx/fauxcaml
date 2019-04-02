e5220e4c51bf3efa403e3ead0066f065e06bfcee
eignnx <eignnx@gmail.com>
2019-04-01 22:35:43 -0400

    Add test for nested `let` expressions

--- fauxcaml/tests/test_compiler_pipeline.py
+++ fauxcaml/tests/test_compiler_pipeline.py
@@ -60,3 +60,16 @@ def test_factorial():
         ;;
         exit (fact 5);;
     """) == 120
+
+
+@build.name_asm_file(__file__)
+def test_nested_let_expr():
+    assert build.exit_code_for("""
+        let my_main x =
+            let y = x + 1 in
+            let z = y + 1 in
+            let w = x + y + z in
+            w
+        ;;
+        exit (my_main 0);;
+    """) == 3
