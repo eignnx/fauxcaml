5f3e6abe7f74716cb8b96186ab19e188051759b7
eignnx <eignnx@gmail.com>
2019-04-17 16:29:38 -0400

    Use tuple unpacking for more readable parse rules

--- fauxcaml/parsing/parser.py
+++ fauxcaml/parsing/parser.py
@@ -24,7 +24,8 @@ pg = rply.ParserGenerator(
 
 @pg.production("stmts : stmt stmts")
 def stmts(s):
-    return syntax.TopLevelStmts([s[0]]) + s[1]
+    stmt, stmts = s
+    return syntax.TopLevelStmts([stmt]) + stmts
 
 
 @pg.production("stmts : stmt")
@@ -44,32 +45,33 @@ def expr_stmt(s):
 
 @pg.production("let_stmt : LET decl SEMI_SEMI")
 def let_stmt(s):
-    decl = s[1]
+    _LET, decl, _SEMI_SEMI = s
     return syntax.LetStmt(decl["lhs"], decl["rhs"])
 
 
 @pg.production("let_stmt : LET REC decl SEMI_SEMI")
 def let_stmt(s):
-    decl = s[2]
+    _LET, _REC, decl, _SEMI_SEMI = s
     return syntax.LetStmt(decl["lhs"], decl["rhs"], recursive=True)
 
 
 @pg.production("expr : IF expr THEN expr ELSE expr")
 def if_expr(s):
-    return syntax.If(s[1], s[3], s[5])
+    _IF, pred, _THEN, yes, _ELSE, no = s
+    return syntax.If(pred, yes, no)
 
 
 @pg.production("expr : LET REC decl IN expr")
 def let_expr(s):
-    decl = s[1]
-    return syntax.Let(decl["lhs"], decl["rhs"], s[3], recursive=True)
+    _LET, _REC, decl, _IN, body = s
+    return syntax.Let(decl["lhs"], decl["rhs"], body, recursive=True)
 
 
 # TODO: Impl type checking for non-rec let exprs.
 @pg.production("expr : LET decl IN expr")
 def let_expr(s):
-    decl = s[1]
-    return syntax.Let(decl["lhs"], decl["rhs"], s[3])
+    _LET, decl, _IN, expr = s
+    return syntax.Let(decl["lhs"], decl["rhs"], expr)
 
 
 @pg.production("decl : IDENT EQ expr")
@@ -82,11 +84,10 @@ def val_decl(s):
 
 @pg.production("decl : IDENT params EQ expr")
 def fun_decl(s):
-    params = s[1]
-    body = s[3]
+    ident, params, _EQ, body = s
     fn = utils.foldr(syntax.Lambda, params + [body])
     return {
-        "lhs": syntax.Ident(s[0].value),
+        "lhs": syntax.Ident(ident.value),
         "rhs": fn,
     }
 
@@ -98,7 +99,8 @@ def params_single(s):
 
 @pg.production("params : params IDENT")
 def params_multi(s):
-    return s[0] + [syntax.Ident(s[1].value)]
+    params, ident = s
+    return params + [syntax.Ident(ident.value)]
 
 
 @pg.production("expr : FUN IDENT ROCKET expr")
@@ -109,47 +111,55 @@ def fn_expr(s):
 
 @pg.production("expr : expr expr", precedence="application")
 def fn_call(s):
-    return syntax.Call(s[0], s[1])
+    fn, arg = s
+    return syntax.Call(fn, arg)
 
 
 @pg.production("expr : INT_LIT")
 def int_lit_expr(s):
-    value = int(s[0].value)
+    [int_lit] = s
+    value = int(int_lit.value)
     return syntax.Const(value, typ.Int)
 
 
 @pg.production("expr : BOOL_LIT")
 def bool_lit_expr(s):
+    [bool_lit] = s
     value = {
         "true": True,
         "false": False
-    }[s[0].value]
+    }[bool_lit.value]
     return syntax.Const(value, typ.Bool)
 
 
 @pg.production("expr : LPAREN tuple_comps RPAREN")
 def tuple_expr(s):
-    return syntax.TupleLit(*s[1])
+    _LPAREN, tuple_comps, _RPAREN = s
+    return syntax.TupleLit(*tuple_comps)
 
 
 @pg.production("tuple_comps : expr COMMA tuple_comps")
 def tuple_components_multi(s):
-    return (s[0], *s[2])
+    expr, _COMMA, tuple_comps = s
+    return (expr, *tuple_comps)
 
 
 @pg.production("tuple_comps : expr COMMA expr")
 def tuple_components_two(s):
-    return (s[0], s[2])
+    expr1, _COMMA, expr2 = s
+    return (expr1, expr2)
 
 
 @pg.production("expr : IDENT")
 def ident_expr(s):
-    return syntax.Ident(s[0].value)
+    [ident] = s
+    return syntax.Ident(ident.value)
 
 
 @pg.production("expr : LPAREN expr RPAREN")
 def paren_expr(s):
-    return s[1]
+    _LPAREN, expr, _RPAREN = s
+    return expr
 
 
 @pg.production("expr : expr PLUS expr")
@@ -159,8 +169,9 @@ def paren_expr(s):
 @pg.production("expr : expr MOD expr")
 @pg.production("expr : expr EQ expr", precedence="infix_operator")
 def bin_op_expr(s):
-    ident = syntax.Ident(s[1].value)
-    return syntax.Call(ident, syntax.TupleLit(s[0], s[2]))
+    expr1, op, expr2 = s
+    ident = syntax.Ident(op.value)
+    return syntax.Call(ident, syntax.TupleLit(expr1, expr2))
 
 
 parser = pg.build()
